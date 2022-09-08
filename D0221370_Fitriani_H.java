1846586474e69baa6f825b676e4f2233d08207fd
Fitriani <100329154+Fitriani-26@users.noreply.github.com>
2022-09-08 20:03:10 +0800

    Add files via upload

---
+++ D0221370_Fitriani_H.java
@@ -0,0 +1,27 @@
+package d0221370_fitriani_h;
+public class D0221370_Fitriani_H {
+    public static void main(String[] args) {
+        String nama = "Fitriani";
+        byte umur = 19 ;
+        int tahun_lahir = 2002;
+        long ukuran_sepatu = 37;
+        short berat_badan = 35;
+        float jarak_rumah = 2.5f;
+        boolean status = true;
+        char jumlah_saudara = '3';
+        double tinggi_badan = 1.4 ;
+        
+        System.out.println("BIODATA MAHASISWA");
+        System.out.println("Nama  : " + nama);
+        System.out.println("Tahun lahir : " + tahun_lahir);
+        System.out.println("Umur : " + umur + " Tahun ");
+        System.out.println("Tinggi badan : " + tinggi_badan  +  " meter ");
+        System.out.println("Berat badan : " + berat_badan + " kg ");
+        System.out.println("Ukuran sepatu : " + ukuran_sepatu);
+        System.out.println("Jumlah saudara : " + jumlah_saudara);
+        System.out.println("Jarak rumah ke Unsulbar : " + jarak_rumah + " km ");
+        System.out.println("Mahasiswa Aktif : " + status);  
+        // TODO code application logic here
+    }
+    
+}
